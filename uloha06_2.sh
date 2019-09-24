c57ab2871e4a20684b13b561f9357e1ecf52b304
N3msix <Sarumanix@gmail.com>
2019-09-24 21:31:35 +0200

    uloha06_2.sh

--- uloha06_2.sh
+++ uloha06_2.sh
@@ -7,13 +7,4 @@ elif [[ "$1" == "countrycodes_en.csv" && "$2" == "kodyzemi_cz.csv" ]]; then
 	EN="/"$1
 fi
 
-cat $CZ | awk '{split($0,a,";"); print a[1],a[2],a[3]}' > /tmp/CZfileTMP
-cat $EN | awk '{split($0,b,";"); print b[4],b[3],b[2]}' > /tmp/ENfileTMP
-
-join <(sort /tmp/CZfileTMP) <(sort /tmp/ENfileTMP) | awk '{split($0,c," "); print c[1]}' > /tmp/EnCzTMP
-
-
-cat $CZ | awk '{split($0,d,";"); print d[1],d[4]}' > /tmp/CZfileTMP
-join <(sort /tmp/CZfileTMP) <(sort /tmp/EnCzTMP) | sed 's/\"//g; s/^[0-9]* //g'
-
-rm -f /tmp/CZfileTMP /tmp/ENfileTMP /tmp/EnCzTMP
+join -1"1" -2"4" -t";" <(sort -t";" -k1 $EN) <(sort -t";" -k4 $CZ)
